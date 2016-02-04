977f1200f8f405d6f43e493a812c52b0e2eaf3e9
Ignat Harczuk <ignathe@gmail.com>
2016-02-04 16:43:30 +0100

    moved loprop.py, daltools, and test into loprop package, and included all tests for setup.py install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 import os
@@ -16,8 +16,23 @@ reqs = ['argparse>=1.2.1',
 
 setup(name="LoProp",
     version="0.1",
-    packages=[".", "test", "daltools", "daltools/util"],
-    scripts=["loprop.py",],
+    packages = find_packages(),
+    package_data = { 'loprop.test' : [ '*/tmp/RSPVEC',
+                                       '*/tmp/DALTON.BAS',
+                                       '*/tmp/AOONEINT',
+                                       '*/tmp/SIRIFC',
+                                       '*/tmp/AOPROPER', ],
+                     'loprop.daltools.test' : ['data/*', 
+                         'test*/RSPVEC',
+                         'test*/DALTON.BAS',
+                         'test*/AOONEINT',
+                         'test*/SIRIFC',
+                         'test*/AOPROPER',
+                         'test*/E3VEC',
+                         ],
+                     'loprop.daltools.util.test' : [ 'fort.1', 'fort.2', 'fort.3'], 
+                     },
+    scripts=["loprop/loprop.py",],
     author="Olav Vahtras",
     author_email="vahtras@kth.se",
     install_requires = reqs,
