309ed6c47b0d70c1e12f0309db3768605afd965e
Olav Vahtras <vahtras@kth.se>
2016-04-05 19:25:42 +0200

    Update setup/reqs

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-import os
-
-#if submodules have not been initiated, just update them
-os.system( 'git submodule update --init --recursive' )
-
+from setuptools import setup
 
 setup(name="LoProp",
     version="0.1.6",
@@ -16,6 +8,6 @@ setup(name="LoProp",
     scripts = ['scripts/loprop'],
     author="Olav Vahtras",
     author_email="vahtras@kth.se",
-    install_requires = ["daltools"],
+    install_requires = ["daltools", "util"],
     description = 'LoProp implementation for Dalton',
     )
