0949a7fd828b2d414861bd208e28760dbd41c3f2
Olav Vahtras <vahtras@kth.se>
2016-09-19 13:54:49 +0200

    Fix versions, split forbin

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="LoProp",
     version="0.2.0",
-    packages = ["loprop"],
-    scripts = ['scripts/loprop'],
+    packages=["loprop"],
+    #packages=find_packages(exclude=("tests",)),
+    scripts=['scripts/loprop'],
     author="Olav Vahtras",
     author_email="vahtras@kth.se",
     install_requires = ["daltools", "util"],
