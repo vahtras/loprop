d512146b36b09b989dc1a10182d988d09417d489
Olav Vahtras <vahtras@kth.se>
2017-02-13 14:55:07 +0100

    new test pff

--- tests/test_bond.py
+++ tests/test_bond.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-str_nobond = """AU
+STR_NOBOND = """AU
 3 1 2 1
 1     0.00000000     0.00000000     0.00000000    -0.66387672     0.00000000    -0.00000000     0.34509720     3.78326969    -0.00000000    -0.00000000     3.96610412     0.00000000     3.52668267     0.00000000    -0.00000000    -2.98430053     0.00000000    -0.00000000     0.00000000    -0.00000000     1.26744725    -0.00000000     2.16730601
 1     1.43043000     0.00000000     1.10716000     0.33193836    -0.16057903    -0.00000000    -0.11299312     1.55235099    -0.00000000     1.15495299     0.60859677    -0.00000000     1.21104235    -4.46820475     0.00000000    -4.55909022    -0.05601735     0.00000000    -3.72029878    -0.00000000     0.46039909    -0.00000000    -2.40410436
@@ -10,7 +10,7 @@ str_nobond = """AU
 Time used in Loprop              :      0.45 (cpu)       0.11 (wall)
 """
 
-str_bond ="""AU
+STR_BOND = """AU
 5 1 22 1
 1     0.00000000     0.00000000     0.00000000    -0.66387672     0.00000000    -0.00000000     0.41788500     1.19165567     0.00000000     0.00000000     2.74891057     0.00000000     1.33653383     0.00000000     0.00000000     4.18425484     0.00000000    -0.00000000    -0.00000000    -0.00000000     0.19037387     0.00000000     5.96033807
 1     0.71521500     0.00000000     0.55358000     0.00000000    -0.06567795    -0.00000000    -0.07278780     2.59161403    -0.00000000     1.21719355     1.98015668    -0.00000000     2.19014883    -7.24839104     0.00000000    -7.16855538     0.59534043     0.00000000    -5.74640170    -0.00000000     1.07707338    -0.00000000    -3.79303206
@@ -22,101 +22,111 @@ Time used in Loprop              :      0.45 (cpu)       0.11 (wall)
 """
 
 class TestBondH2O(unittest.TestCase):
+    """H2O tests bonded versus non-bonden results"""
 
 
     def setUp(self):
 #Read in string that is for no bonds output
-        lines = [line for line in str_bond.split('\n') if len(line.split()) > 10 ]
+        lines = [line for line in STR_BOND.split('\n') if len(line.split()) > 10]
         a0 = 1.0
 
-        self.n_bond = np.array(   [8.0, 0.0, 1.0, 0.0, 1.0], dtype = float )
-        self.r_bond = a0 * np.array(   [l.split()[1:4] for l in lines ], dtype = float)
-        self.q_bond = np.array(   [l.split()[4] for l in lines], dtype = float)
-        self.d_bond = np.array(   [l.split()[5:8] for l in lines], dtype = float)
-        self.a_bond = np.array(   [l.split()[8:15] for l in lines], dtype = float)
-        self.b_bond = np.array(   [l.split()[15:26] for l in lines], dtype = float)
+        self.n_bond = np.array([8.0, 0.0, 1.0, 0.0, 1.0], dtype=float)
+        self.r_bond = a0 * np.array([l.split()[1:4] for l in lines], dtype=float)
+        self.q_bond = np.array([l.split()[4] for l in lines], dtype=float)
+        self.d_bond = np.array([l.split()[5:8] for l in lines], dtype=float)
+        self.a_bond = np.array([l.split()[8:15] for l in lines], dtype=float)
+        self.b_bond = np.array([l.split()[15:26] for l in lines], dtype=float)
 
-        self.coc_bond = np.einsum( 'ij,i', self.r_bond , self.n_bond ) / self.n_bond.sum()
+        self.coc_bond = np.einsum('ij,i', self.r_bond, self.n_bond) / self.n_bond.sum()
 
 #Read in string that is for bonds output -b
-        lines = [line for line in str_nobond.split('\n') if len(line.split()) > 10 ]
+        lines = [line for line in STR_NOBOND.split('\n') if len(line.split()) > 10]
 
-        self.n_nobond = np.array( [8.0, 1.0, 1.0], dtype = float )
-        self.r_nobond = a0 * np.array( [l.split()[1:4] for l in lines ], dtype = float)
-        self.q_nobond = np.array( [l.split()[4] for l in lines], dtype = float)
-        self.d_nobond = np.array( [l.split()[5:8] for l in lines], dtype = float)
-        self.a_nobond = np.array( [l.split()[8:15] for l in lines], dtype = float)
-        self.b_nobond = np.array( [l.split()[15:26] for l in lines], dtype = float)
+        self.n_nobond = np.array([8.0, 1.0, 1.0], dtype=float)
+        self.r_nobond = a0 * np.array([l.split()[1:4] for l in lines], dtype=float)
+        self.q_nobond = np.array([l.split()[4] for l in lines], dtype=float)
+        self.d_nobond = np.array([l.split()[5:8] for l in lines], dtype=float)
+        self.a_nobond = np.array([l.split()[8:15] for l in lines], dtype=float)
+        self.b_nobond = np.array([l.split()[15:26] for l in lines], dtype=float)
 
-        self.coc_nobond = np.einsum( 'ij,i', self.r_nobond , self.n_nobond ) / self.n_nobond.sum()
+        self.coc_nobond = np.einsum('ij,i', self.r_nobond, self.n_nobond) / self.n_nobond.sum()
 
 
     def test_bond_nobond_properties(self):
-        np.testing.assert_allclose(self.coc_bond, self.coc_nobond )
+        """Center-of-charge equality"""
+        np.testing.assert_allclose(self.coc_bond, self.coc_nobond)
 
     def test_a(self):
+        """Polarizability equality"""
         a_tot_bond = np.sum(self.a_bond)
         a_tot_nobond = np.sum(self.a_nobond)
 
-        np.testing.assert_allclose(a_tot_bond, a_tot_nobond )
+        np.testing.assert_allclose(a_tot_bond, a_tot_nobond)
 
     def test_b(self):
+        """Hyperpolarizability equality"""
         b_tot_bond = np.sum(self.b_bond)
         b_tot_nobond = np.sum(self.b_nobond)
 
-        np.testing.assert_allclose(b_tot_bond, b_tot_nobond )
+        np.testing.assert_allclose(b_tot_bond, b_tot_nobond)
 
     def test_dip(self):
-        dip_bond = np.einsum( 'ij,i', (self.r_bond - self.coc_bond), self.q_bond ) + self.d_bond.sum(axis=0)
-        dip_nobond = np.einsum( 'ij,i', (self.r_nobond - self.coc_nobond), self.q_nobond ) + self.d_nobond.sum(axis = 0 )
+        """Dipole equality"""
+        dip_bond = np.einsum('ij,i', (self.r_bond - self.coc_bond), self.q_bond) + self.d_bond.sum(axis=0)
+        dip_nobond = np.einsum('ij,i', (self.r_nobond - self.coc_nobond), self.q_nobond) + self.d_nobond.sum(axis=0)
         np.testing.assert_allclose(dip_bond, dip_nobond)
 
 class TestBondH2S(unittest.TestCase):
+    """H2O tests bonded versus non-bonden results"""
 
 
     def setUp(self):
 #Read in string that is for no bonds output
-        lines = [line for line in str_bond.split('\n') if len(line.split()) > 10 ]
+        lines = [line for line in STR_BOND.split('\n') if len(line.split()) > 10]
         a0 = 1.0
 
-        self.n_bond = np.array(   [16.0, 0.0, 1.0, 0.0, 1.0], dtype = float )
-        self.r_bond = a0 * np.array(   [l.split()[1:4] for l in lines ], dtype = float)
-        self.q_bond = np.array(   [l.split()[4] for l in lines], dtype = float)
-        self.d_bond = np.array(   [l.split()[5:8] for l in lines], dtype = float)
-        self.a_bond = np.array(   [l.split()[8:15] for l in lines], dtype = float)
-        self.b_bond = np.array(   [l.split()[15:26] for l in lines], dtype = float)
+        self.n_bond = np.array([16.0, 0.0, 1.0, 0.0, 1.0], dtype=float)
+        self.r_bond = a0 * np.array([l.split()[1:4] for l in lines], dtype=float)
+        self.q_bond = np.array([l.split()[4] for l in lines], dtype=float)
+        self.d_bond = np.array([l.split()[5:8] for l in lines], dtype=float)
+        self.a_bond = np.array([l.split()[8:15] for l in lines], dtype=float)
+        self.b_bond = np.array([l.split()[15:26] for l in lines], dtype=float)
 
-        self.coc_bond = np.einsum( 'ij,i', self.r_bond , self.n_bond ) / self.n_bond.sum()
+        self.coc_bond = np.einsum('ij,i', self.r_bond, self.n_bond) / self.n_bond.sum()
 
 #Read in string that is for bonds output -b
-        lines = [line for line in str_nobond.split('\n') if len(line.split()) > 10 ]
+        lines = [line for line in STR_NOBOND.split('\n') if len(line.split()) > 10]
 
-        self.n_nobond = np.array( [16.0, 1.0, 1.0], dtype = float )
-        self.r_nobond = a0 * np.array( [l.split()[1:4] for l in lines ], dtype = float)
-        self.q_nobond = np.array( [l.split()[4] for l in lines], dtype = float)
-        self.d_nobond = np.array( [l.split()[5:8] for l in lines], dtype = float)
-        self.a_nobond = np.array( [l.split()[8:15] for l in lines], dtype = float)
-        self.b_nobond = np.array( [l.split()[15:26] for l in lines], dtype = float)
+        self.n_nobond = np.array([16.0, 1.0, 1.0], dtype=float)
+        self.r_nobond = a0 * np.array([l.split()[1:4] for l in lines], dtype=float)
+        self.q_nobond = np.array([l.split()[4] for l in lines], dtype=float)
+        self.d_nobond = np.array([l.split()[5:8] for l in lines], dtype=float)
+        self.a_nobond = np.array([l.split()[8:15] for l in lines], dtype=float)
+        self.b_nobond = np.array([l.split()[15:26] for l in lines], dtype=float)
 
-        self.coc_nobond = np.einsum( 'ij,i', self.r_nobond , self.n_nobond ) / self.n_nobond.sum()
+        self.coc_nobond = np.einsum('ij,i', self.r_nobond, self.n_nobond) / self.n_nobond.sum()
 
 
     def test_bond_nobond_properties(self):
-        np.testing.assert_allclose(self.coc_bond, self.coc_nobond )
+        """Center-of-charge equality"""
+        np.testing.assert_allclose(self.coc_bond, self.coc_nobond)
 
     def test_a(self):
+        """Polarizability equality"""
         a_tot_bond = np.sum(self.a_bond)
         a_tot_nobond = np.sum(self.a_nobond)
 
-        np.testing.assert_allclose(a_tot_bond, a_tot_nobond )
+        np.testing.assert_allclose(a_tot_bond, a_tot_nobond)
 
     def test_b(self):
+        """Hyperpolarizability equality"""
         b_tot_bond = np.sum(self.b_bond)
         b_tot_nobond = np.sum(self.b_nobond)
 
-        np.testing.assert_allclose(b_tot_bond, b_tot_nobond )
+        np.testing.assert_allclose(b_tot_bond, b_tot_nobond)
 
     def test_dip(self):
-        dip_bond = np.einsum( 'ij,i', (self.r_bond - self.coc_bond), self.q_bond ) + self.d_bond.sum(axis=0)
-        dip_nobond = np.einsum( 'ij,i', (self.r_nobond - self.coc_nobond), self.q_nobond ) + self.d_nobond.sum(axis = 0 )
+        """Dipole equality"""
+        dip_bond = np.einsum('ij,i', (self.r_bond - self.coc_bond), self.q_bond) + self.d_bond.sum(axis=0)
+        dip_nobond = np.einsum('ij,i', (self.r_nobond - self.coc_nobond), self.q_nobond) + self.d_nobond.sum(axis=0)
         np.testing.assert_allclose(dip_bond, dip_nobond)
---
+++ tests/test_pff.py
@@ -0,0 +1,28 @@
+from .common import loprop, LoPropTestCase
+import unittest, os
+from loprop.core import MolFrag, penalty_function, shift_function
+
+case = "pff"
+DIR = os.path.join(case, 'tmp')
+exec('from . import %s_data as ref'%case)
+
+class SulphurTest(LoPropTestCase):
+
+    def setUp(self):
+        self.tmp = os.path.join( os.path.dirname( __file__ ), DIR)
+        self.mf = MolFrag( tmpdir = self.tmp, max_l =0, pol = 0,
+                freqs = None,
+                pf = penalty_function(2.0),
+                sf = shift_function,
+                gc = None)
+        self.maxDiff = None
+
+    def test_dir(self):
+        self.assertTrue(os.path.isdir(self.tmp))
+
+    def test_nuclear_charge(self):
+        Z = self.mf.Z
+        self.assert_allclose(Z, ref.Z)
+
+if __name__ == '__main__':#pragma: no cover
+    unittest.main()
