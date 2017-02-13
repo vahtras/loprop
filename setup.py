944a26dfc3b7a9bfd57ef7f03d4371d978664a94
Olav Vahtras <vahtras@kth.se>
2017-02-13 17:45:35 +0100

    release 0.2.2
    Identical to 0.2.1 but uploaded to pypi as sdist only
    bdist_wheels generated loprop script with local hardcoded python paths

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="LoProp",
-    version="0.2.1",
+    version="0.2.2",
     packages=["loprop"],
     #packages=find_packages(exclude=("tests",)),
     scripts=['scripts/loprop'],
