fdc9930f1487a2f615f4f114bc12e27a386ef81f
Olav Vahtras <vahtras@kth.se>
2019-05-24 10:31:43 +0200

    0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="LoProp",
-    version="0.2.2",
+    version="0.2.3",
     packages=["loprop"],
     #packages=find_packages(exclude=("tests",)),
     scripts=['scripts/loprop'],
