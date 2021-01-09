4505e88854150408774cb1644d277942599f2b84
Olav Vahtras <vahtras@kth.se>
2021-01-09 12:13:09 +0100

    0.3.0; make daltools optional dependency

--- loprop/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import MolFrag, penalty_function
-from .dalton import MolFragDalton
-from .veloxchem import MolFragVeloxChem
--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from setuptools import setup
 
 setup(
     name="LoProp",
-    version="0.2.4",
+    version="0.3.0",
     packages=["loprop"],
     entry_points={
         'console_scripts': ['loprop=loprop.__main__:main'],
     },
     author="Olav Vahtras",
     author_email="vahtras@kth.se",
-    install_requires=["h5py", "daltools"],
+    install_requires=["blocked-matrix-utils"],
+    extras_require={
+        "dalton": ["daltools"],
+        "vlx": ["h5py"],
+    },
     description='LoProp implementation for Dalton',
     )
