cadca07214934c4ea3d51c62718a37e697ba97f7
Olav Vahtras <vahtras@kth.se>
2021-01-26 11:47:48 +0100

    0.3.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="LoProp",
-    version="0.3.2",
+    version="0.3.3",
     packages=["loprop"],
     entry_points={
         'console_scripts': ['loprop=loprop.__main__:main'],
